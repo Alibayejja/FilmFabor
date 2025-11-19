85a42735fbbe8b1c02130914e2c2b40dcd1d0fa1
Alibayejja <alibayejja604@gmail.com>
2025-11-19 15:34:01 +0000

    feat: Rename Images

---
+++ src/data/movies.js
@@ -0,0 +1,282 @@
+export const movies = [
+  {
+    id: 1,
+    title: "The Fantastic Four",
+    year: 2025,
+    image: "public/images/img 1.jpeg",
+    description:
+      "Forced to balance their roles as heroes with their family bond, the Fantastic Four must defend Earth from a cosmic threat known as Galactus.",
+    country: "United States of America",
+    genre: "Sci-Fi, Fantasy",
+    releaseDate: "2025-11-14",
+    duration: "97 min",
+    rating: 7.9,
+    cast: ["Pedro Pascal", "Vanessa Kirby", "Ebon Moss-Bachrach"]
+  },
+  {
+    id: 2,
+    title: "Deadpool & Wolverine",
+    year: 2024,
+    image: "public/images/img 2.jpeg",
+    description:
+      "Deadpool joins forces with Wolverine in a chaotic multiverse mission to save everything they care about.",
+    country: "USA",
+    genre: "Action, Comedy",
+    releaseDate: "2024-07-26",
+    duration: "126 min",
+    rating: 8.2,
+    cast: ["Ryan Reynolds", "Hugh Jackman"]
+  },
+  {
+    id: 3,
+    title: "Dune: Part Two",
+    year: 2024,
+    image: "public/images/img 3.jpeg",
+    description:
+      "Paul Atreides unites with the Fremen and prepares a war against the Emperor and House Harkonnen.",
+    country: "USA",
+    genre: "Sci-Fi, Adventure",
+    releaseDate: "2024-03-01",
+    duration: "166 min",
+    rating: 8.8,
+    cast: ["Timothée Chalamet", "Zendaya", "Rebecca Ferguson"]
+  },
+  {
+    id: 4,
+    title: "Avatar: The Way of Water",
+    year: 2022,
+    image: "public/images/img 4.jpeg",
+    description:
+      "Jake Sully and Neytiri protect their family against a new human threat exploring Pandora's oceans.",
+    country: "USA",
+    genre: "Sci-Fi, Fantasy",
+    releaseDate: "2022-12-16",
+    duration: "192 min",
+    rating: 7.6,
+    cast: ["Sam Worthington", "Zoe Saldana"]
+  },
+  {
+    id: 5,
+    title: "Interstellar",
+    year: 2014,
+    image: "public/images/img 5.jpeg",
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    country: "USA",
+    genre: "Sci-Fi, Drama",
+    releaseDate: "2014-11-07",
+    duration: "169 min",
+    rating: 8.6,
+    cast: ["Matthew McConaughey", "Anne Hathaway"]
+  },
+  {
+    id: 6,
+    title: "Inception",
+    year: 2010,
+    image: "public/images/img 6.jpeg",
+    description:
+      "A thief who enters people's dreams must perform the most difficult mission: planting an idea.",
+    country: "USA",
+    genre: "Sci-Fi, Action",
+    releaseDate: "2010-07-16",
+    duration: "148 min",
+    rating: 8.8,
+    cast: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"]
+  },
+  {
+    id: 7,
+    title: "Oppenheimer",
+    year: 2023,
+    image: "public/images/img 7.jpeg",
+    description:
+      "The story of J. Robert Oppenheimer, the father of the atomic bomb, and the moral dilemmas he faced.",
+    country: "USA",
+    genre: "Biography, Drama",
+    releaseDate: "2023-07-21",
+    duration: "180 min",
+    rating: 8.6,
+    cast: ["Cillian Murphy", "Emily Blunt", "Robert Downey Jr."]
+  },
+  {
+    id: 8,
+    title: "John Wick: Chapter 4",
+    year: 2023,
+    image: "public/images/img 8.jpeg",
+    description:
+      "John Wick faces his most powerful enemies across the globe as he continues his fight for freedom.",
+    country: "USA",
+    genre: "Action, Thriller",
+    releaseDate: "2023-03-24",
+    duration: "169 min",
+    rating: 7.8,
+    cast: ["Keanu Reeves", "Donnie Yen"]
+  },
+  {
+    id: 9,
+    title: "Spider-Man: No Way Home",
+    year: 2021,
+    image: "public/images/img 9.jpeg",
+    description:
+      "Peter Parker seeks help from Doctor Strange to make people forget his identity, but things go wrong.",
+    country: "USA",
+    genre: "Action, Adventure",
+    releaseDate: "2021-12-17",
+    duration: "148 min",
+    rating: 8.3,
+    cast: ["Tom Holland", "Zendaya", "Benedict Cumberbatch"]
+  },
+  {
+    id: 10,
+    title: "The Batman",
+    year: 2022,
+    image: "public/images/img 10.jpeg",
+    description:
+      "Batman investigates a series of murders while facing the Riddler in a dark and gritty Gotham.",
+    country: "USA",
+    genre: "Action, Crime",
+    releaseDate: "2022-03-04",
+    duration: "176 min",
+    rating: 7.8,
+    cast: ["Robert Pattinson", "Zoë Kravitz"]
+  },
+  {
+    id: 11,
+    title: "Joker",
+    year: 2019,
+    image: "public/images/img 11.jpeg",
+    description:
+      "Arthur Fleck descends into madness and becomes the criminal mastermind known as the Joker.",
+    country: "USA",
+    genre: "Crime, Drama",
+    releaseDate: "2019-10-04",
+    duration: "122 min",
+    rating: 8.4,
+    cast: ["Joaquin Phoenix", "Robert De Niro"]
+  },
+  {
+    id: 12,
+    title: "Guardians of the Galaxy Vol. 3",
+    year: 2023,
+    image: "public/images/img 12.jpeg",
+    description:
+      "The Guardians face a final mission to save Rocket from his mysterious past.",
+    country: "USA",
+    genre: "Action, Sci-Fi",
+    releaseDate: "2023-05-05",
+    duration: "150 min",
+    rating: 8.1,
+    cast: ["Chris Pratt", "Zoe Saldana"]
+  },
+  {
+    id: 13,
+    title: "Mission Impossible: Dead Reckoning",
+    year: 2023,
+    image: "public/images/img 13.jpeg",
+    description:
+      "Ethan Hunt faces a new threat: a mysterious AI capable of global chaos.",
+    country: "USA",
+    genre: "Action, Thriller",
+    releaseDate: "2023-07-12",
+    duration: "163 min",
+    rating: 7.7,
+    cast: ["Tom Cruise", "Hayley Atwell"]
+  },
+  {
+    id: 14,
+    title: "Black Panther",
+    year: 2018,
+    image: "public/images/img 14.jpeg",
+    description:
+      "T’Challa returns to Wakanda as king but faces an enemy from within his own nation.",
+    country: "USA",
+    genre: "Action, Fantasy",
+    releaseDate: "2018-02-16",
+    duration: "134 min",
+    rating: 7.3,
+    cast: ["Chadwick Boseman", "Michael B. Jordan"]
+  },
+  {
+    id: 15,
+    title: "The Matrix Resurrections",
+    year: 2021,
+    image: "public/images/img 15.jpeg",
+    description:
+      "Neo returns to a new version of the Matrix and faces his past and destiny once again.",
+    country: "USA",
+    genre: "Sci-Fi, Action",
+    releaseDate: "2021-12-22",
+    duration: "148 min",
+    rating: 6.1,
+    cast: ["Keanu Reeves", "Carrie-Anne Moss"]
+  },
+  {
+    id: 16,
+    title: "Kung Fu Panda 4",
+    year: 2024,
+    image: "public/images/img 16.jpeg",
+    description:
+      "Po embarks on a new adventure to discover the next Dragon Warrior while facing a new villain.",
+    country: "USA",
+    genre: "Animation, Comedy",
+    releaseDate: "2024-03-08",
+    duration: "94 min",
+    rating: 7.1,
+    cast: ["Jack Black", "Awkwafina"]
+  },
+  {
+    id: 17,
+    title: "Venom: Let There Be Carnage",
+    year: 2021,
+    image: "public/images/img 17.jpeg",
+    description:
+      "Eddie Brock and Venom face a dangerous new symbiote known as Carnage.",
+    country: "USA",
+    genre: "Action, Sci-Fi",
+    releaseDate: "2021-10-01",
+    duration: "97 min",
+    rating: 6.0,
+    cast: ["Tom Hardy", "Woody Harrelson"]
+  },
+  {
+    id: 18,
+    title: "Doctor Strange in the Multiverse of Madness",
+    year: 2022,
+    image: "public/images/img 18.jpeg",
+    description:
+      "Doctor Strange travels across the multiverse facing alternate realities and Wanda's dark powers.",
+    country: "USA",
+    genre: "Action, Fantasy",
+    releaseDate: "2022-05-06",
+    duration: "126 min",
+    rating: 7.0,
+    cast: ["Benedict Cumberbatch", "Elizabeth Olsen"]
+  },
+  {
+    id: 19,
+    title: "Fast X",
+    year: 2023,
+    image: "public/images/img 19.jpeg",
+    description:
+      "Dom Toretto and his family face a vengeful foe connected to their troubled past.",
+    country: "USA",
+    genre: "Action, Crime",
+    releaseDate: "2023-05-19",
+    duration: "141 min",
+    rating: 5.9,
+    cast: ["Vin Diesel", "Jason Momoa"]
+  },
+  {
+    id: 20,
+    title: "The Marvels",
+    year: 2023,
+    image: "https://image.tmdb.org/t/p/w500/Arhp5xY4P0m0mGxC77x4ssQqHgM.jpg",
+    description:
+      "Captain Marvel, Ms. Marvel, and Monica Rambeau team up to save the universe from a new threat.",
+    country: "USA",
+    genre: "Action, Sci-Fi",
+    releaseDate: "2023-11-10",
+    duration: "105 min",
+    rating: 6.0,
+    cast: ["Brie Larson", "Iman Vellani"]
+  }
+];
